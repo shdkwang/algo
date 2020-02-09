3c90ce9f51073cb79e03afaada2010cbf0340a5e
shdkw <419316333@qq.com>
2020-02-10 02:41:52 +0800

    heapify

--- src/main/java/com/dk/sort/heap/MaxHeap.java
+++ src/main/java/com/dk/sort/heap/MaxHeap.java
@@ -14,6 +14,23 @@ public class MaxHeap<Item extends Comparable> {
         this.capacity = capacity;
     }
 
+    // 构造函数, 通过一个给定数组创建一个最大堆
+    // 该构造堆的过程, 时间复杂度为O(n)
+    public MaxHeap(Item arr[]){
+
+        int n = arr.length;
+
+        data = (Item[])new Comparable[n+1];
+        capacity = n;
+
+        for( int i = 0 ; i < n ; i ++ )
+            data[i+1] = arr[i];
+        count = n;
+
+        for( int i = count/2 ; i >= 1 ; i -- )
+            shiftDown(i);
+    }
+
     // 返回堆中的元素个数
     public int size(){
         return count;
@@ -31,6 +48,24 @@ public class MaxHeap<Item extends Comparable> {
         data[count+1] = item;
         count ++;
         shiftUp(count);
+    }
+
+    // 从最大堆中取出堆顶元素, 即堆中所存储的最大数据
+    public Item extractMax(){
+        assert count > 0;
+        Item ret = data[1];
+
+        swap( 1 , count );
+        count --;
+        shiftDown(1);
+
+        return ret;
+    }
+
+    // 获取最大堆中的堆顶元素
+    public Item getMax(){
+        assert( count > 0 );
+        return data[1];
     }
 
 
@@ -52,15 +87,40 @@ public class MaxHeap<Item extends Comparable> {
         }
     }
 
+    private void shiftDown(int k){
+
+        while( 2*k <= count ){
+            int j = 2*k; // 在此轮循环中,data[k]和data[j]交换位置
+            if( j+1 <= count && data[j+1].compareTo(data[j]) > 0 )
+                j ++;
+            // data[j] 是 data[2*k]和data[2*k+1]中的最大值
+
+            if( data[k].compareTo(data[j]) >= 0 ) break;
+            swap(k, j);
+            k = j;
+        }
+    }
+
     // 测试 MaxHeap
     public static void main(String[] args) {
 
         MaxHeap<Integer> maxHeap = new MaxHeap<Integer>(100);
-        int N = 50; // 堆中元素个数
+        int N = 100; // 堆中元素个数
         int M = 100; // 堆中元素取值范围[0, M)
         for( int i = 0 ; i < N ; i ++ )
             maxHeap.insert( new Integer((int)(Math.random() * M)) );
-        System.out.println(maxHeap.size());
 
+        Integer[] arr = new Integer[N];
+        // 将maxheap中的数据逐渐使用extractMax取出来
+        // 取出来的顺序应该是按照从大到小的顺序取出来的
+        for( int i = 0 ; i < N ; i ++ ){
+            arr[i] = maxHeap.extractMax();
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // 确保arr数组是从大到小排列的
+        for( int i = 1 ; i < N ; i ++ )
+            assert arr[i-1] >= arr[i];
     }
 }
