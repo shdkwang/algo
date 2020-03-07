7853742e666c58d22c47c95cdc0ea7098086486e
shdkw <419316333@qq.com>
2020-03-07 23:48:07 +0800

    knapsack

--- src/main/java/com/dk/leetcode/dp/Knapsack/Solution1.java
+++ src/main/java/com/dk/leetcode/dp/Knapsack/Solution1.java
@@ -5,43 +5,38 @@ package com.dk.leetcode.dp.Knapsack;
 /// 时间复杂度: O(n * C) 其中n为物品个数; C为背包容积
 /// 空间复杂度: O(n * C)
 public class Solution1 {
-
     private int[][] memo;
 
-    public int knapsack01(int[] w, int[] v, int C){
+    public int knapsack01(int[] weight, int[] value, int capacity) {
+        if (weight == null || value == null || weight.length != value.length)
+            throw new IllegalArgumentException("Invalid weight or value");
 
-        if(w == null || v == null || w.length != v.length)
-            throw new IllegalArgumentException("Invalid w or v");
+        if (capacity < 0)
+            throw new IllegalArgumentException("capacity must be greater than or equal to zero.");
 
-        if(C < 0)
-            throw new IllegalArgumentException("C must be greater or equal to zero.");
-
-        int n = w.length;
-        if(n == 0 || C == 0)
+        int n = weight.length;
+        if (n == 0 || capacity == 0)
             return 0;
 
-        memo = new int[n][C + 1];
-        return bestValue(w, v, n - 1, C);
+        memo = new int[n][capacity + 1];
+        return bestValue(weight, value, n-1, capacity);
     }
 
-    // 用 [0...index]的物品,填充容积为c的背包的最大价值
-    private int bestValue(int[] w, int[] v, int index, int c){
-
-        if(c <= 0 || index < 0)
+    // 用 [0...index]的物品,填充容积为capacity的背包的最大价值
+    private int bestValue(int[] weight, int[] value, int index, int capacity) {
+        if (capacity <= 0 || index < 0)
             return 0;
 
-        if(memo[index][c] != -1)
-            return memo[index][c];
+        if (memo[index][capacity] != -1)
+            return memo[index][capacity];
 
-        int res = bestValue(w, v, index-1, c);
-        if(c >= w[index])
-            res = Math.max(res, v[index] + bestValue(w, v, index - 1, c - w[index]));
+        int res = bestValue(weight, value, index-1, capacity);
+        if (capacity >= weight[index]) {
+            res = Math.max(res, value[index] + bestValue(weight, value, index - 1, capacity - weight[index]));
+            memo[index][capacity] = res;
+        }
 
-        return memo[index][c] = res;
-    }
-
-    public static void main(String[] args) {
-
+        return res;
     }
 
 }
