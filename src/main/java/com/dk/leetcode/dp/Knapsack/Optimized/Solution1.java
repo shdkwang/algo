6e0ce706355b1318fdcd77211fa560915c8cb196
shdkw <419316333@qq.com>
2020-03-08 01:07:27 +0800

    knapsack

---
+++ src/main/java/com/dk/leetcode/dp/Knapsack/Optimized/Solution1.java
@@ -0,0 +1,40 @@
+package com.dk.leetcode.dp.Knapsack.Optimized;
+
+public class Solution1 {
+    public int knapsack01(int[] weight, int[] value, int capacity) {
+        if (weight == null || value == null || weight.length != value.length)
+            throw new IllegalArgumentException("Invalid weight or value");
+
+        if (capacity < 0)
+            throw new IllegalArgumentException("capacity must be greater than or equal to zero.");
+
+        int n = weight.length;
+        if (n == 0 || capacity == 0)
+            return 0;
+
+        int[][] memo = new int[2][capacity + 1];
+
+        for (int j = 0; j <= capacity; j++) {
+            memo[0][j] = ( j >= weight[0] ? value[0] : 0);
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j<= capacity; j++) {
+                memo[i%2][j] = memo[(i-1)%2][j];
+                if (j >= weight[i]) {
+                    memo[i%2][j] = Math.max(memo[i%2][j], value[i] + memo[(i-1)%2][j - weight[i]]);
+                }
+            }
+        }
+
+        return memo[(n-1)%2][capacity];
+    }
+
+    public static void main(String[] args) {
+        int[] weight = new int[]{1, 2, 3};
+        int[] value = new int[]{6, 10, 12};
+        int capacity = 5;
+        Solution1 solution1 = new Solution1();
+        System.out.println(solution1.knapsack01(weight, value, capacity));
+    }
+}
